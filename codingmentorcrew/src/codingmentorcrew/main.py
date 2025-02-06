55d543fb1c997bdc625a246585366e53861d8cf8
FranzShih <c22522443@mytudublin.ie>
2025-02-06 14:10:42 +0000

    test success

--- codingmentorcrew/src/codingmentorcrew/main.py
+++ codingmentorcrew/src/codingmentorcrew/main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from codingmentorcrew.crew import Codingmentorcrew
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
-"testing"
+
 # This main file is intended to be a way for you to run your
 # crew locally, so refrain from adding unnecessary logic into this file.
 # Replace with inputs you want to test with, it will automatically
