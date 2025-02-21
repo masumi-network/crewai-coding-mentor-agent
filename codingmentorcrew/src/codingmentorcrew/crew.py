52e4557f6cbce8ac59f433c408517bfc84454e6d
Franz Shih <C22522443@mytudublin.ie>
2025-02-21 12:53:06 +0000

    created working retriever -> scraper sequence

--- codingmentorcrew/src/codingmentorcrew/crew.py
+++ codingmentorcrew/src/codingmentorcrew/crew.py
@@ -1,19 +1,21 @@
-from crewai import Agent, Crew, Process, Task
+from crewai import Crew, Process, Agent, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai_tools import ScrapeWebsiteTool
+from crewai_tools import SerperDevTool
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from crewai_tools import FileWriterTool
 import os
+from crewai_tools import FileWriterTool
 
 load_dotenv()
 Model = 'gpt-4o'
 api_key = os.getenv('OPENAI_API_KEY')
 llm = ChatOpenAI(model = Model,api_key=api_key)
 
-
-site = 'https://www.geeksforgeeks.org/merge-sort/'
-tool = ScrapeWebsiteTool(website_url=site)
+crawlTool = SerperDevTool(
+	n_results=2
+)
 
 file_writer = FileWriterTool()
 
@@ -34,36 +36,61 @@ class Codingmentorcrew():
 	# If you would like to add tools to your agents, you can learn more about it here:
 	# https://docs.crewai.com/concepts/agents#agent-tools
 	@agent
+	def retriever(self) -> Agent:
+		return Agent(
+			config=self.agents_config['retriever'],
+			verbose=True,
+			llm = llm
+		)
+	@agent
 	def scraper(self) -> Agent:
 		return Agent(
 			config=self.agents_config['scraper'],
-			tools=[tool],
+			tools=[ScrapeWebsiteTool()],
 			verbose=True,
 			llm = llm
 		)
 
+	
+	
+
 	# To learn more about structured task outputs, 
 	# task dependencies, and task callbacks, check out the documentation:
 	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
+
+	@task
+	def retriever_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['retriever_task'],
+			agent = self.retriever(),
+			output_key="retrieved_url"
+		)
+	
 	@task
 	def scraper_task(self) -> Task:
 		return Task(
 			config=self.tasks_config['scrape_task'],
-			output_file = 'data.txt'
+			output_file = 'data.txt',
+			agent=self.scraper(),
+			depends_on=[self.retriever_task()],
+			inputs={"website_url": "{{retriever_task.retrieved_url}}"}
 		)
+
+
+
 		
 	@crew
 	def crew(self) -> Crew:
 		"""Creates the ReferenceCrew crew"""
 		# To learn how to add knowledge sources to your crew, check out the documentation:
 		# https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
+		
 		return Crew(
 			agents=self.agents,  # Automatically created by the @agent decorator
-			tasks=self.tasks,  # Automatically created by the @task decorator
+			tasks=[self.retriever_task(),self.scraper_task()],  # Automatically created by the @task decorator
+			##manager_llm=ChatOpenAI(temperature=0, model="gpt-4o"),
 			process=Process.sequential,
 			verbose=True,
-			# process=Process.hierarchical,  # Alternative process mode
+			#manager_agent=None,
 		)
-	#	def fileWrite(input):
-			#file_writer._run('example.txt', input, 'C:\Users\natha\Desktop\Baxtos Kebab')	
+	
--- codingmentorcrew/src/codingmentorcrew/main.py
+++ codingmentorcrew/src/codingmentorcrew/main.py
@@ -4,6 +4,12 @@ import warnings
 from datetime import datetime
 from codingmentorcrew.crew import Codingmentorcrew
 import tiktoken
+from codingmentorcrew.crew import crawlTool
+from codingmentorcrew.crew import file_writer
+import os
+
+
+
 enc = tiktoken.encoding_for_model("gpt-4o")
 MAX_TOKENS = 8192
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
@@ -17,15 +23,16 @@ def run():
     """
     Run the crew.
     """
-    
+    chud = str(crawlTool.run(search_query= str(input("Enter search query: "))))
     inputs = {
-        'current_year': str(datetime.now().year)
+        'topic': chud,
+        'current_year': str(datetime.now().year),
     }
     try:
-        #result = Codingmentorcrew().crew().kickoff()
-        
-        print(str(Codingmentorcrew().crew().kickoff()))
+        result = Codingmentorcrew().crew().kickoff(inputs = inputs)
+        #print(str(Codingmentorcrew().crew().kickoff()))
 
-        
+        print(str(result))
+
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
